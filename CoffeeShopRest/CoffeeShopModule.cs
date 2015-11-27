ca694a02152d056b65685bd2d7fd4839b53dc17c
mikehell68 <mikehell68@gmail.com>
2015-11-27 17:07:25 +0000

    modified some test pages

--- CoffeeShopRest/CoffeeShopModule.cs
+++ CoffeeShopRest/CoffeeShopModule.cs
@@ -1,22 +1,16 @@
-﻿using System.Threading.Tasks;
-using CoffeeShopRest.BusinessLayer;
+﻿using CoffeeShopRest.BusinessLayer;
 using log4net;
 using Nancy;
-using Nancy.Security;
-
 
 namespace CoffeeShopRest
 {
     public class CoffeeShopModule: NancyModule
     {
         private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        private readonly IMembershipService _membership;
         private readonly DataBaseService _dataBaseService = new DataBaseService();
         
-        public CoffeeShopModule(IMembershipService membership)
+        public CoffeeShopModule()
         {
-            _membership = membership;
-
             Get["/"] = parameters =>
             {
                 Log.InfoFormat("Get request {0}", Request.Url);
@@ -24,14 +18,7 @@ namespace CoffeeShopRest
 
                 return View["views/CoffeeShop/StaticView", allCompanies];
             };
-
-
-            Get["/testing"] = parameters =>
-            {
-                Log.InfoFormat("Get request {0}", Request.Url);
-                return View["staticview", Request.Url];
-            };
-
+            
             Get["/map"] = parameters =>
             {
                 Log.InfoFormat("Get request {0}", Request.Url);
@@ -41,9 +28,6 @@ namespace CoffeeShopRest
             Get["/register"] = _ =>
             {
                 Log.InfoFormat("Get request {0}", Request.Url);
-                //already logged in
-                if (Context.CurrentUser.IsAuthenticated())
-                    return Response.AsRedirect("~/");
                 return View["views/CoffeeShop/NewUser"];
             };
 
@@ -53,37 +37,12 @@ namespace CoffeeShopRest
                 var registerResult = _dataBaseService.AddCompany(Request.Form.Name, Request.Form.Description);
                 
                 return LoginAndRedirect(registerResult);
-
             };
         }
 
         private object LoginAndRedirect(dynamic company)
         {
             return View["views/CoffeeShop/Welcome", company];
-        }
-    }
-
-    public class MissingUserIdentity
-    {
-    }
-
-    public interface IMembershipService
-    {
-        Task<bool> IsUserNameAvailable(string username);
-
-        Task<NewUser> AddUser(string userName, string email, string password);
-    }
-
-    class MembershipService : IMembershipService
-    {
-        public async Task<bool> IsUserNameAvailable(string username)
-        {
-            return await Task.Run(() => true);
-        }
-
-        public async Task<NewUser> AddUser(string userName, string email, string password)
-        {
-            return await Task.Run(() => new NewUser {UserName = userName, Email = email, Password = password});
         }
     }
 }
